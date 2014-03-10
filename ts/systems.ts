61c0735c88660613031573203ac05fdc90e0c99b
giraluna <giraluna@users.noreply.github.com>
2014-03-10 13:03:32 +0200

    base class for systems

--- ts/systems.ts
+++ ts/systems.ts
@@ -15,7 +15,7 @@
   * @property accumulated Amount of time banked towards next tick
   * 
   */
-class SystemsManager extends PIXI.EventTarget
+class SystemsManager
 {
   systems: any = {};
   timer: Strawb.Timer;
@@ -25,8 +25,6 @@ class SystemsManager extends PIXI.EventTarget
 
   constructor(tickTime)
   {
-    super();
-
     this.timer = new Strawb.Timer();
     this.tickTime = tickTime / 1000;
   }
@@ -40,7 +38,6 @@ class SystemsManager extends PIXI.EventTarget
     this.tickNumber++;
     for (var system in this.systems)
     {
-
       this.systems[system].tick(this.tickNumber);
     }
   }
@@ -51,5 +48,56 @@ class SystemsManager extends PIXI.EventTarget
     {
       this.tick();
     }
+  }
+}
+
+class System
+{
+  activationRate: number;
+  lastTick: number;
+  nextTick: number;
+
+  activate: () => any;
+
+  constructor(activationRate: number, currTick: number)
+  {
+    this.activationRate = activationRate;
+    this.updateTicks(currTick);
+
+    if (activationRate < 1)
+    {
+      console.log("<1 activationRate on system", this);
+    }
+  }
+  updateTicks(currTick: number)
+  {
+    this.lastTick = currTick;
+    this.nextTick = currTick + this.activationRate;
+  }
+
+  tick(currTick: number)
+  {
+    if (currTick + this.activationRate >= this.nextTick)
+    {
+      // do something
+      this.activate();
+      
+      this.updateTicks(currTick);
+    }
+  }
+}
+
+class ProfitSystem extends System
+{
+  targets: any[] = [];
+
+  constructor(activationRate: number, currTick: number)
+  {
+    super(activationRate, currTick);
+  }
+
+  activate()
+  {
+    
   }
 }
